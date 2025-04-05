4de59281750af45745a6d95f6daa5050e0534788
Nomlas <161704576+nomlasvrc@users.noreply.github.com>
2025-04-05 11:50:26 +0900

    不要なコメントを削除

--- Packages/com.nomlas.topazchat/Editor/AutoPauseEditor.cs
+++ Packages/com.nomlas.topazchat/Editor/AutoPauseEditor.cs
@@ -20,20 +20,14 @@ namespace Nomlas.TopazChat
         {
             serializedObject.Update();
 
-            // スライダーの最小値(0より大きい値)と最大値の設定
             float minValue = 0.1f;
             float maxValue = 100f;
 
-            // pauseDistenceのスライダー
-            // pauseDistenceはminValueからmaxValueまで調整可能
             pauseDistance.floatValue = EditorGUILayout.Slider("Pause Distence", Mathf.Max(pauseDistance.floatValue, minValue), minValue, maxValue);
 
-            // resumeDistanceのスライダー
-            // resumeDistanceはminValueから(pauseDistence-小さな余裕値)まで調整可能とする
             float resumeMax = pauseDistance.floatValue - 0.1f;
             resumeDistance.floatValue = EditorGUILayout.Slider("Resume Distance", Mathf.Max(resumeDistance.floatValue, minValue), minValue, resumeMax);
 
-            // 万が一、resumeDistanceがpauseDistence以上になってしまった場合は補正
             if (resumeDistance.floatValue >= pauseDistance.floatValue)
             {
                 resumeDistance.floatValue = pauseDistance.floatValue - 0.1f;
