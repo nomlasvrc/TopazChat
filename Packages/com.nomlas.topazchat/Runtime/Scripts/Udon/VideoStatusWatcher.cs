d2787ef5dce1275ccb2cd0bf43ad1600ac1487f6
Nomlas <161704576+nomlasvrc@users.noreply.github.com>
2026-02-27 20:40:05 +0900

    好みの問題

--- Packages/com.nomlas.topazchat/Runtime/Scripts/Udon/VideoStatusWatcher.cs
+++ Packages/com.nomlas.topazchat/Runtime/Scripts/Udon/VideoStatusWatcher.cs
@@ -4,13 +4,13 @@ namespace Nomlas.TopazChat
 {
     public class VideoStatusWatcher : PlayerEventListener
     {
-        public override void OnVideoEnd() { player.PlayerVideoEnd(); }
-        public override void OnVideoError(VideoError videoError) { player.PlayerVideoError(videoError); }
-        public override void OnVideoLoop() { player.PlayerVideoLoop(); }
-        public override void OnVideoPause() { player.PlayerVideoPause(); }
-        public override void OnVideoPlay() { player.PlayerVideoPlay(); }
-        public override void OnVideoReady() { player.PlayerVideoReady(); }
-        public override void OnVideoStart() { player.PlayerVideoStart(); }
+        public override void OnVideoEnd() => player.PlayerVideoEnd();
+        public override void OnVideoError(VideoError videoError) => player.PlayerVideoError(videoError);
+        public override void OnVideoLoop() => player.PlayerVideoLoop();
+        public override void OnVideoPause() => player.PlayerVideoPause();
+        public override void OnVideoPlay() => player.PlayerVideoPlay();
+        public override void OnVideoReady() => player.PlayerVideoReady();
+        public override void OnVideoStart() => player.PlayerVideoStart();
 
         public override string GetListenerName()
         {
