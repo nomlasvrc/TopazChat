1f009e47bc828ec1220df1131084357b3109c042
Nomlas <161704576+nomlasvrc@users.noreply.github.com>
2026-03-09 14:28:30 +0900

    リファクタリング

--- Packages/com.nomlas.topazchat/Editor/TopazChatPlayerEditor.cs
+++ Packages/com.nomlas.topazchat/Editor/TopazChatPlayerEditor.cs
@@ -20,38 +20,34 @@ namespace Nomlas.TopazChat
         {
             TopazChatPlayer player = (TopazChatPlayer)target;
 
+            if (player.controller == null)
+            {
+                EditorGUILayout.HelpBox("コントローラーが見つかりません。一部機能が無効化されます。", MessageType.Warning);
+                return;
+            }
+
             var oldStreamKey = streamKey;
             streamKey = EditorGUILayout.TextField("ストリームキー", streamKey);
             EditorGUILayout.HelpBox("このテキストフィールドに入力すると、StreamURLが自動で更新されます。", MessageType.None);
-
-            EditorGUILayout.Space();
-
-            if (player.controller != null)
+            if (oldStreamKey != streamKey)
             {
-                /*
-                bool oldAndroidMode = player.controller.androidMode;
-                player.controller.androidMode = EditorGUILayout.Toggle("Quest/Android向けURL変更機能", player.controller.androidMode);
-                EditorGUILayout.HelpBox("VRChatの制限により、Quest/Android向けのURL（rtsp://topaz.chat/live/XXX）を手動で入力する必要があります。この機能がオフになっていてもデフォルトのストリームURLはプラットフォーム別に使用されます。", MessageType.Info);
-                if (oldAndroidMode != player.controller.androidMode)
-                {
-                    EditorUtility.SetDirty(player.controller);
-                }
-                */
+                player.defaultStreamURL = new VRCUrl($"{TopazUtils.TopazURL}/{streamKey}");
+                player.defaultStreamURL_Android = new VRCUrl($"{TopazUtils.TopazURL_Android}/{streamKey}");
+                if (player.controller.address != null) player.controller.address.text = streamKey;
 
-                if (oldStreamKey != streamKey)
-                {
-                    player.defaultStreamURL = new VRCUrl($"{TopazUtils.TopazURL}/{streamKey}");
-                    player.defaultStreamURL_Android = new VRCUrl($"{TopazUtils.TopazURL_Android}/{streamKey}");
-                    if (player.controller.address != null) player.controller.address.text = streamKey;
-
-                    EditorUtility.SetDirty(target);
-                    EditorUtility.SetDirty(player.controller.address);
-                }
+                EditorUtility.SetDirty(target);
+                EditorUtility.SetDirty(player.controller.address);
             }
-            else
+
+            /*
+            bool oldAndroidMode = player.controller.androidMode;
+            player.controller.androidMode = EditorGUILayout.Toggle("Quest/Android向けURL変更機能", player.controller.androidMode);
+            EditorGUILayout.HelpBox("VRChatの制限により、Quest/Android向けのURL（rtsp://topaz.chat/live/XXX）を手動で入力する必要があります。この機能がオフになっていてもデフォルトのストリームURLはプラットフォーム別に使用されます。", MessageType.Info);
+            if (oldAndroidMode != player.controller.androidMode)
             {
-                EditorGUILayout.HelpBox("コントローラーが見つかりません。一部機能が無効化されます。", MessageType.Warning);
+                EditorUtility.SetDirty(player.controller);
             }
+            */
         }
     }
 }
