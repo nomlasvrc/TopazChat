685d7f3cbb96ef582d24f4dc70e7eaf9324280ac
Nomlas <161704576+nomlasvrc@users.noreply.github.com>
2026-05-21 23:55:11 +0900

    コメント追加

--- Packages/com.nomlas.topazchat/Runtime/Scripts/Modules/AutoPause.cs
+++ Packages/com.nomlas.topazchat/Runtime/Scripts/Modules/AutoPause.cs
@@ -8,15 +8,24 @@ namespace Nomlas.TopazChat
         public override string ListenerName => "AutoPause";
         [SerializeField] internal float pauseDistance = 40;
         [SerializeField] internal float resumeDistance = 38;
+        /// <summary>
+        /// 一時停止する距離の二乗
+        /// </summary>
         private float sqrPauseDistance;
+        /// <summary>
+        /// 再生を再開する距離の二乗
+        /// </summary>
         private float sqrResumeDistance;
         private bool isPausing;
         private VRCPlayerApi local;
         protected override void Start()
         {
             base.Start();
+
+            // 距離の二乗を計算して保存
             sqrPauseDistance = pauseDistance * pauseDistance;
             sqrResumeDistance = resumeDistance * resumeDistance;
+
             local = Networking.LocalPlayer;
             isPausing = false;
         }
@@ -24,15 +33,17 @@ namespace Nomlas.TopazChat
         private void Update()
         {
             var playerPos = local.GetPosition();
-            var checkPos = this.transform.position;
+            var checkPos = this.transform.position;//一応持ち運び可能なようにする
             float sqrDistance = (checkPos - playerPos).sqrMagnitude;
             if (sqrDistance > sqrPauseDistance)
             {
+                // 一時停止中でないなら一時停止する
                 if (!isPausing) player.Pause();
                 isPausing = true;
             }
             else if (sqrDistance < sqrResumeDistance)
             {
+                // 一時停止中なら再開する
                 if (isPausing) player.Resume();
                 isPausing = false;
             }
--- Packages/com.nomlas.topazchat/Runtime/Scripts/Modules/Save.cs
+++ Packages/com.nomlas.topazchat/Runtime/Scripts/Modules/Save.cs
@@ -15,9 +15,19 @@ namespace Nomlas.TopazChat
         [UdonSynced] private VRCUrl savedUrl;
         [UdonSynced] private VRCUrl savedUrl_Android;
 
+        /// <summary>
+        /// 保存されたURLを返します。
+        /// </summary>
         [PublicAPI] public VRCUrl SavedURL => savedUrl;
+        /// <summary>
+        /// 保存されたURL(Android)を返します。
+        /// </summary>
         [PublicAPI] public VRCUrl SavedURL_Android => savedUrl_Android;
 
+        /// <summary>
+        /// URLを保存します。
+        /// 内部用
+        /// </summary>
         public void _SaveKey(VRCUrl url, VRCUrl url_Android)
         {
             savedUrl = url;
